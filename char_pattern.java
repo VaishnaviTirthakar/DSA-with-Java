740373a1328a30a3c8bbb390f7ccd3c26eaaf111
VaishnaviTirthakar <vaishavitirthakar11@gmail.com>
2026-09-23 00:49:24 +0530

    pattern java programs

---
+++ char_pattern.java
@@ -0,0 +1,30 @@
+public class char_pattern {
+    public static void main(String args[]){
+    int n=5;
+    char ch='A';
+
+        for(int l=1;l<=n;l++){
+            for(int i=1;i<=l;i++){
+                  System.out.print(ch);
+                    //ch++;
+                }
+
+            System.out.println();
+            //ch++;
+         } 
+         ch++;
+
+
+
+         /*for(int l=1;l<=n;l++){
+            for(int i=1;i<=n-l+1;i++){
+                  System.out.print(ch);
+                    ch++;
+                }
+
+            System.out.println();
+         } */
+
+    }
+  
+}
--- pattern.java
+++ pattern.java
@@ -1,3 +1,4 @@
+//print pattern
 public class pattern {
     public static void main (String arg[]){
         for(int i=1; i<=4; i++){
@@ -9,3 +10,20 @@ public class pattern {
     }
     
 }
+
+
+//print inverted pattern
+public static void main (String arg[]){
+    int n=4;
+    for(int i=1;i<=4;i++){
+        for(int j=1;i<=(n-i+1);i++){
+            System.out.print("*");
+
+        }
+        System.out.println();
+
+    }
+}
+
+
+
