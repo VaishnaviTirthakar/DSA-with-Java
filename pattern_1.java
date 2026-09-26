15cf169a285bf36b3db9f6c1b3c01724f5f7fb40
VaishnaviTirthakar <vaishavitirthakar11@gmail.com>
2026-09-27 01:57:00 +0530

    Inverted & Rotated Half-Pyramid

---
+++ pattern_1.java
@@ -0,0 +1,30 @@
+public class pattern_1 {
+
+    public static void patt(int n){
+        
+        for(int i=1; i<=n; i++){
+
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+
+
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            
+            System.out.println();
+
+
+        }
+
+        
+
+    }
+
+    public static void main(String[] args) {
+        patt(5);
+        
+    }
+    
+}
